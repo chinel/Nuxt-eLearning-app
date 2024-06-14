99c650dfe38f55d923209fd174c4f744fdd0d0be
chinel <nelokwaz@gmail.com>
2024-06-14 08:32:42 +0100

    feat: create a composable to return course data

---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,25 @@
+<template>
+  <h2>Lesson</h2>
+  <p>This is a lesson</p>
+  <div>
+    <p>Chapter - {{ chapter.title }}</p>
+    <p>Lesson - {{ lesson.title }}</p>
+  </div>
+</template>
+
+<script setup>
+const course = useCourse();
+const route = useRoute();
+
+const chapter = computed(() => {
+  return course.chapters.find(
+    (chapter) => chapter.slug === route.params.chapterSlug
+  );
+});
+const lesson = computed(() => {
+  return chapter.value.lessons.find((lesson) => {
+    console.log(lesson);
+    return lesson.slug === route.params.lessonSlug;
+  });
+});
+</script>
