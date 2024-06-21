5b75efc60de7bee3ffa5a7292f2f92263148f42f
chinel <nelokwaz@gmail.com>
2024-06-21 07:39:05 +0100

    fix: add active link stlyles & refactoring

--- pages/course.vue
+++ pages/course.vue
@@ -25,8 +25,13 @@
             v-for="(lesson, index) in chapter.lessons"
             :key="lesson.slug"
             class="flex flex-row space-x-1 no-underline prose-sm font-normal py-1 px-4 -mx-4"
-            :to="`/course/chapter/${chapter.slug}/lesson/${lesson.slug}`"
+            :to="lesson.path"
+            :class="{
+              'text-blue-500': lesson.path === $route.fullPath,
+              'text-gray-600': lesson.path !== $route.fullPath,
+            }"
           >
+            >
             <span class="text-gray-500"> {{ index + 1 }} </span>
             <span>{{ lesson.title }} </span>
           </NuxtLink>
@@ -43,3 +48,9 @@
 <script setup>
 const { chapters } = useCourse();
 </script>
+
+<!-- <style scoped>
+.router-link-active {
+  @apply text-blue-500;
+}
+</style> -->
