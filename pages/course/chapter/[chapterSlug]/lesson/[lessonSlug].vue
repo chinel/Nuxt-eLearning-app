addafb1844a36d270974311e710d61bea073a850
chinel <nelokwaz@gmail.com>
2024-07-01 13:42:48 +0100

    feat: add logic to handle server side error using route validation

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -38,18 +38,43 @@
 const course = useCourse();
 const route = useRoute();
 
+definePageMeta({
+  validate({ params }) {
+    const course = useCourse();
+    const chapter = course.chapters.find(
+      (chapter) => chapter.slug === params.chapterSlug
+    );
+    if (!chapter) {
+      return createError({
+        statusCode: 404,
+        message: "Chapter not found",
+      });
+    }
+    const lesson = chapter.lessons.find(
+      (lesson) => lesson.slug === params.lessonSlug
+    );
+    if (!lesson) {
+      return createError({
+        statusCode: 404,
+        message: "Lesson not found",
+      });
+    }
+    return true;
+  },
+});
+
 const chapter = computed(() => {
   return course.chapters.find(
     (chapter) => chapter.slug === route.params.chapterSlug
   );
 });
 
-if (!chapter.value) {
-  throw createError({
-    statusCode: 404,
-    message: "Chapter not found",
-  });
-}
+// if (!chapter.value) {
+//   throw createError({
+//     statusCode: 404,
+//     message: "Chapter not found",
+//   });
+// }
 
 const lesson = computed(() => {
   return chapter.value.lessons.find((lesson) => {
@@ -58,12 +83,12 @@ const lesson = computed(() => {
   });
 });
 
-if (!lesson.value) {
-  throw createError({
-    statusCode: 404,
-    message: "Lesson not found",
-  });
-}
+// if (!lesson.value) {
+//   throw createError({
+//     statusCode: 404,
+//     message: "Lesson not found",
+//   });
+// }
 
 const title = computed(() => {
   return `${lesson.value.title} - ${course.title}`;
