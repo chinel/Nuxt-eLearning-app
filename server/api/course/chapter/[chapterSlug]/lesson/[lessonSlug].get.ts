29149435bff0a24f63d7b1d96771a783b3602903
chinel <nelokwaz@gmail.com>
2024-08-29 08:53:33 +0100

    feat: add logic to retrieve lesson data from DB

--- server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
+++ server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
@@ -1,31 +1,12 @@
+import { PrismaClient } from "~/prisma/client/index.js";
 import course from "~/server/courseData";
 import type { Chapter, Course, Lesson, LessonWithPath } from "~/types/course";
 
 course as Course;
 
-export default defineEventHandler((event): LessonWithPath => {
-  const { chapterSlug, lessonSlug } = event.context.params!;
-  const chapter: Maybe<Chapter> = course.chapters.find(
-    (chapter) => chapter.slug === chapterSlug
-  );
-  if (!chapter) {
-    throw createError({
-      statusCode: 404,
-      message: "Chapter not found",
-    });
-  }
-  const lesson: Maybe<Lesson> = chapter.lessons.find(
-    (lesson) => lesson.slug === lessonSlug
-  );
-  if (!lesson) {
-    throw createError({
-      statusCode: 404,
-      message: "Lesson not found",
-    });
-  }
+const prisma = new PrismaClient();
 
-  return {
-    ...lesson,
-    path: `/course/chapter/${chapterSlug}/lesson/${lessonSlug}`,
-  };
+export default defineEventHandler((event) => {
+  const { chapterSlug, lessonSlug } = event.context.params!;
+  return prisma.lesson.findFirst();
 });
